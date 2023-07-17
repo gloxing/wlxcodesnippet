2e768e765b8f98782bd7ba99850472a77ef79739
0027021234 <weng.longxing@iwhalecloud.com>
2023-07-17 16:45:58 +0800

    threejs

---
+++ css/.history/threejs/threejs-demo/src/main_20230717154135.ts
@@ -0,0 +1,35 @@
+import './style.css'
+import * as THREE from 'three';
+
+// init
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+ 
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true,alpha: true  } );
+renderer.setSize( 200,200 );
+renderer.setAnimationLoop( animation );
+
+//render 背景色设置透明
+
+
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
---
+++ css/.history/threejs/threejs-demo/src/main_20230717154310.ts
@@ -0,0 +1,33 @@
+import './style.css'
+import * as THREE from 'three';
+
+// init
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+camera.position.z = 5;
+
+const scene = new THREE.Scene();
+ 
+
+const geometry = new THREE.BoxGeometry( 1, 1,1 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true,alpha: true  } );
+renderer.setSize( window.innerWidth,window.innerHeight );
+renderer.setAnimationLoop( animation );
+
+
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
